94553e8b356786a9d9c5cc120f3dc52cc34efcba
rauldangio <jojofagqwrt@gmail.com>
2023-02-19 21:42:30 -0300

    adicionando a agenda

--- agenda.py
+++ agenda.py
@@ -1,1 +1,17 @@
-print('hello world!')
+agenda = {
+    "joao":{
+        "tel": "99312-3131",
+        "email": "joao@email.com",
+        "endereco": "av. 1"
+    },
+    "maria": {
+        "tel": "78954-6241",
+        "email": "maria@email.com",
+        "endereco": "av. 2"
+    },
+    "raul": {
+        "tel": "21233-6542",
+        "email": "raul@email.com",
+        "endereco": "av. 3"
+    }
+}
