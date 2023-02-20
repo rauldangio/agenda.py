8520a28cf81cf9d05c9f5c38c9d2fcd809ed1375
rauldangio <jojofagqwrt@gmail.com>
2023-02-19 22:42:50 -0300

    adding the show_all_schedule method

--- agenda.py
+++ agenda.py
@@ -1,5 +1,9 @@
 SCHEDULE = {
-  
+    "pedrinho":{
+        "Telefone": "93132-4123",
+        "Email": "pedrinho@solyd.com",
+        "Endereço": "Não tem"
+    }
 }
 
 
@@ -8,15 +12,20 @@ def read_contact(name):
     for info in SCHEDULE[name]:
         print(f"\t{info}: {SCHEDULE[name][info]}")
 
+def show_all_schedule():
+    for contact in SCHEDULE:
+        print(contact.upper())
+        for info in SCHEDULE[contact]:
+            print(f"\t{info}: {SCHEDULE[contact][info]}")        
+
 
 def create_contact():
     name = input("nome do contato: ")
     SCHEDULE[name]={
-        "tel": input(f"telefone do {name}: "),
-        "email": input(f"email do {name}: "),
-        "endereco": input(f"endereco do {name}: ")
+        "Telefone": input(f"telefone do {name}: "),
+        "Email": input(f"email do {name}: "),
+        "Endereco": input(f"endereco do {name}: ")
     }
     print("\ncontato criado\n")
-    read_contact(name)
 
-create_contact()
+show_all_schedule()
