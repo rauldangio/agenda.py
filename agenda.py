eb8fbe47e410b1039e6b3a31920767050b70b790
rauldangio <jojofagqwrt@gmail.com>
2023-02-20 12:20:32 -0300

    error treatment

--- agenda.py
+++ agenda.py
@@ -8,11 +8,15 @@ SCHEDULE = {
 
 
 def search_contact(name):
-    print('-'*30)
-    print(name.upper()+":")
-    for info in SCHEDULE[name]:
-        print(f"\t{info}: {SCHEDULE[name][info]}")
-    print('-'*30)
+    print('-'*40)
+    try:
+        SCHEDULE[name]
+        print(name.upper()+":")
+        for info in SCHEDULE[name]:
+            print(f"\t{info}: {SCHEDULE[name][info]}")
+        print('-'*40)
+    except:
+        print('>>>>>>Esse contato não existe!')
 
 
 def show_all_schedule():
@@ -21,36 +25,51 @@ def show_all_schedule():
 
 
 def create_contact():
+    
     name = input("nome do contato: ")
-    SCHEDULE[name]={
+    try:
+        SCHEDULE[name]
+        print('-'*40)
+        print(">>>>>>contato já existe")
+    except:
+        SCHEDULE[name]={
         "Telefone": input(f"telefone do {name}: "),
         "Email": input(f"email do {name}: "),
         "Endereco": input(f"endereco do {name}: ")
-    }
-    print(f"\n>>>>>> contato {name} criado\n")
+        }
+        print(f"\n>>>>>> contato {name} criado\n")
 
 
 def update_contact(name):
-    print('oque deseja mudar?')
-    upd = int(input(f"1 -> telefone\n2 -> email\n3 -> endereço\n4 -> tudo\n5 -> não quero mudar nada\nopção: "))
-    if upd == 1:
-        SCHEDULE[name]['Telefone'] = input("novo telefone: ")
-    elif upd == 2:
-        SCHEDULE[name]['Email'] = input("novo email: ")
-    elif upd == 3:
-        SCHEDULE[name]['Endereco'] = input("novo endereco: ")
-    elif upd == 4:
-        SCHEDULE[name]['Telefone'] = input("novo telefone: ")
-        SCHEDULE[name]['Email'] = input("novo email: ")
-        SCHEDULE[name]['Endereco'] = input("novo endereco: ")
-    elif upd == 5:
-        pass
-    print(f">>>>>> contato {name} atualizado")
+    print('-'*40)
+    try:
+        SCHEDULE[name]
+        print('oque deseja mudar?')
+        upd = int(input(f"1 -> telefone\n2 -> email\n3 -> endereço\n4 -> tudo\n5 -> não quero mudar nada\nopção: "))
+        if upd == 1:
+            SCHEDULE[name]['Telefone'] = input("novo telefone: ")
+        elif upd == 2:
+            SCHEDULE[name]['Email'] = input("novo email: ")
+        elif upd == 3:
+            SCHEDULE[name]['Endereco'] = input("novo endereco: ")
+        elif upd == 4:
+            SCHEDULE[name]['Telefone'] = input("novo telefone: ")
+            SCHEDULE[name]['Email'] = input("novo email: ")
+            SCHEDULE[name]['Endereco'] = input("novo endereco: ")
+        elif upd == 5:
+            pass
+        print(f">>>>>> contato {name} atualizado")
+    except:
+        print(">>>>>> contato não existe")
+
 
 def delete_contact(name):
-    SCHEDULE.pop(name)
-    print(f">>>>>> contato {name} removido")
-
+    print('-'*40)
+    try:
+        SCHEDULE.pop(name)
+        print(f">>>>>> contato {name} removido")
+    except KeyError:
+        print(">>>>>> esse contato não existe!")
 
 def show_menu():
     print('-'*40)
@@ -65,17 +84,22 @@ def show_menu():
 
 while True:
     show_menu()
-    op = int(input('escolha uma opção: '))
-    if op == 1:
-        create_contact()
-    elif op == 2:
-        search_contact(input("contado: "))
-    elif op == 3:
-        show_all_schedule()
-    elif op == 4:
-        delete_contact(input("contado: "))
-    elif op == 5:
-        contact = input("contato: ")
-        update_contact(contact)
-    elif op == 6:
-        break
+    try:
+        op = int(input('escolha uma opção: '))
+        if op == 1:
+            create_contact()
+        elif op == 2:
+            search_contact(input("contado: "))
+        elif op == 3:
+            show_all_schedule()
+        elif op == 4:
+            delete_contact(input("contado: "))
+        elif op == 5:
+            contact = input("contato: ")
+            update_contact(contact)
+        elif op == 6:
+            break
+        else:
+            print('>>>>>> opção invalida!')
+    except ValueError:
+        print("digite um numero!")
